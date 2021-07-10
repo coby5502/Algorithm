41a9c0328dd5765df04269cb36509832ae42f9f1
Doyoung <coby5502@gmail.com>
2021-07-10 21:36:54 +0900

    vscode setting

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "input numbers" << endl;
+    int x;
+    cin >> x;
+    cout << "hello : " << x << endl;
+    return 0;
+}
